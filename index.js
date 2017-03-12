8f4a19872856346bbc94969d0d3af29220a45720
L.Carquet <laetitiacarquet@gmail.com>
2017-03-12 10:47:42 +0100

    creates an endpoint with GET

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.get('/hola/:name', (req, res) => {
+	res.send({message: `hello ${req.params.name}`})
+})
+
 app.listen(port, () =>{
 	console.log(`api rest corriendo en http localhost:${port}`)
 })
