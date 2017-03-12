002744f62efb8cc545ed41f3ab6e796ae1ecfcc6
L.Carquet <laetitiacarquet@gmail.com>
2017-03-12 11:06:02 +0100

    creates the all the endpoints ofr the four verbs and checks GET and POST

--- index.js
+++ index.js
@@ -9,8 +9,28 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/hola/:name', (req, res) => {
-	res.send({message: `hello ${req.params.name}`})
+app.get('/api/product', (req,res)=> {
+	//callback function
+	res.status(200).send({product: []})
+
+} )
+
+app.get('/api/product/:productId', (req, res) => {
+	//callback function
+})
+
+app.post('/api/product', (req, res) => {
+	//callback function
+	console.log(req.body)
+	res.status(200).send({message: 'your product was received'})
+})
+
+app.put('api/product/:productId', (req, res) => {
+
+})
+
+app.delete('api/product/:productId', (req, res) => {
+
 })
 
 app.listen(port, () =>{
